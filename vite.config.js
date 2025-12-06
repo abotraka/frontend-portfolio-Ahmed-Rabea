dbb533cf3e3b3f50874910e5a6aef0a0819f2671
Ahmed Rabea <159713142+abotraka@users.noreply.github.com>
2025-12-07 00:36:27 +0200

    Update Vite config to change base path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@/src': '/src',
-    },
-  },
-});
-
-
+  base: "/frontend-portfolio-Ahmed-Rabea/",
+})
